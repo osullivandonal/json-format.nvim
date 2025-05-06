574357314e8dd7a5bb44a4cb165ee23af235ad6b
Dónal O'Sullivan <donal.osullivan@wpengine.com>
2025-05-06 09:20:17 +0100

    feat: :sparkles: Give name to tool in the lazyvim menu and give description to each command

--- lua/json-format/init.lua
+++ lua/json-format/init.lua
@@ -3,6 +3,11 @@ local M = {}
 function M.setup(opts)
 	opts = opts or {}
 
+	-- Register 'j' prefix description for LazyVim
+	require("which-key").add({
+		["<leader>j"] = { name = "JSON Formatter" },
+	})
+
 	-- Expand JSON (pretty-print)
 	function M.expand_json()
 		vim.cmd("%!jq .")
@@ -14,8 +19,18 @@ function M.setup(opts)
 	end
 
 	-- Add leader key mappings for JSON formatting
-	vim.keymap.set("n", opts.expand_key or "<leader>je", M.expand_json, { noremap = true, silent = true })
-	vim.keymap.set("n", opts.minify_key or "<leader>jm", M.minify_json, { noremap = true, silent = true })
+	vim.keymap.set(
+		"n",
+		opts.expand_key or "<leader>je",
+		M.expand_json,
+		{ noremap = true, silent = true, desc = "Expand JSON (json-formatter)" }
+	)
+	vim.keymap.set(
+		"n",
+		opts.minify_key or "<leader>jm",
+		M.minify_json,
+		{ noremap = true, silent = true, desc = "Minify JSON (json-formatter)" }
+	)
 end
 
 return M
